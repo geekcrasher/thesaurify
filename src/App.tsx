cb9d122b4ef4af0b25532b0f5232753c1f23b338
Paulo Lopez <geekcrasher@gmail.com>
2024-04-23 16:03:01 +0800

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <main className="min-h-screen w-full flex flex-col p-4">
+      <Button className="w-20 h-8">
+        Hello
+      </Button>
+    </main>
+  )
+}
+
+export default App
