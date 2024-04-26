529a77c98a2a785410d2216f30630efeb9981489
Paulo Lopez <geekcrasher@gmail.com>
2024-04-26 12:51:36 +0800

    refactor: wrap the component inside the WordContextProvider

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,14 +1,17 @@
 import Navbar from '@/components/Navbar'
 import SearchBar from '@/components/Searchbar';
+import { WordContextProvider } from '@/context/WordContext';
+import WordContent from '@/components/Content';
 
 const Home = () => {
   return (
-    <>
-      <section className='flex flex-col gap-6 w-[50rem] max-md:w-full border min-h-[50rem] max-h-fit relative'>
+    <WordContextProvider>
+      <section className='flex flex-col gap-6 w-[50rem] max-md:w-full min-h-[50rem] max-h-fit relative font-lora'>
         <Navbar />
         <SearchBar />
+        <WordContent />
       </section>
-    </>
+    </WordContextProvider>
   );
 }
 
