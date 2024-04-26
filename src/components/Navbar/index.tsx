2f5c75d74bc612d7ac208d5418fc5508cc075a7a
Paulo Lopez <geekcrasher@gmail.com>
2024-04-26 20:30:42 +0800

    style: added the name for the app

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,16 +6,18 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import { Separator } from "@/components/ui/separator"
 import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
 
 const Navbar = () => {
   return (
     <>
-      <nav className="flex items-center justify-between h-16 border">
-        <BookType className="h-10 w-10 text-[#a445ed]" />
-        <section className="flex gap-6">
+      <nav className="flex items-center justify-between h-fit py-1">
+        <h2 className="flex items-center gap-2  cursor-pointer font-lora font-semibold text-[#a445ed]">
+          <BookType className="" />
+          Thesaurify
+        </h2>
+        <section className="flex items-center gap-8">
           <Select>
             <SelectTrigger className="w-[120px] border-0">
               <SelectValue placeholder="Typeface" />
@@ -26,13 +28,13 @@ const Navbar = () => {
               <SelectItem value="system">Monospace</SelectItem>
             </SelectContent>
           </Select>
-          <Separator orientation="vertical" />
-          <div className="flex items-center space-x-4">
+          <div className="h-8 w-[1px] bg-gray-200"></div>
+          <section className="flex items-center space-x-4">
             <Switch id="dark-mode" />
-            <Label htmlFor="dark-mode">
-              <Moon className="w-6 h-6 text-gray-400" />
+            <Label htmlFor="dark-mode" className="cursor-pointer">
+              <Moon className="w-5 h-5 text-gray-400" />
             </Label>
-          </div>
+          </section>
         </section>
       </nav>
     </>
