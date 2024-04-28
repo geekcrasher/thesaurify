c39d65f916a947afa37088a770487606148111ca
Paulo Lopez <geekcrasher@gmail.com>
2024-04-28 21:30:03 +0800

    feat: utilize the useNavigate hook from react router dom for route navigation

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -8,13 +8,11 @@ import {
   FormControl,
   FormField,
   FormItem,
-  // FormDescription,
-  // FormLabel,
-  // FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react";
 import { useWord } from "@/context/WordContext";
+import { useNavigate } from "react-router-dom";
 
 
 const formSchema = z.object({
@@ -26,6 +24,7 @@ const formSchema = z.object({
 const SearchBar = () => {
 
   const { setKeyword } = useWord()
+  const navigate = useNavigate()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -37,6 +36,7 @@ const SearchBar = () => {
   const onSubmit = (values: z.infer<typeof formSchema>): void => {
     try {
       setKeyword(values.word)
+      navigate(`/search`)
     } catch (error) {
       console.log(error)
     }
@@ -45,17 +45,18 @@ const SearchBar = () => {
   return (
     <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="font-inter w-[22rem] sm:w-[28rem] md:w-[35rem]">
           <FormField
             control={form.control}
             name="word"
             render={({ field }) => (
               <FormItem>
                 <FormControl >
-                  <div className="flex w-full h-fit relative">
+                  <div className="flex h-fit relative">
                     <Input
-                      className="h-14 rounded-xl border-0 text-[#333] bg-gray-100/60 text-lg font-bold placeholder:text-base placeholder:text-gray-400 placeholder:font-normal"
+                      className="h-14 rounded-xl border-0 text-[#333]/80 bg-gray-100/80 text-base md:text-lg font-bold placeholder:text-sm sm:placeholder:text-md placeholder:text-gray-400 placeholder:font-normal"
                       placeholder="Search Dictionary"
+                      autoComplete="off"
                       {...field}
                     />
                     <Search className="absolute right-8 top-4 text-[#a445ed]" />
