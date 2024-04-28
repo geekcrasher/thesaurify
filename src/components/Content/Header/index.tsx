6e49d2b41b041e484a8d5224d5aede17088e99c9
Paulo Lopez <geekcrasher@gmail.com>
2024-04-28 21:34:19 +0800

    refactor: accessed the wordInfoData instead of wordInfo from WordContext

--- src/components/Content/Header/index.tsx
+++ src/components/Content/Header/index.tsx
@@ -9,21 +9,21 @@ type HeaderProps = {
 
 const Header = ({ isAudioPlay, mp3Source, onClick }: HeaderProps) => {
 
-  const { wordInfo } = useWord()
+  const { wordInfoData } = useWord()
 
   return (
     <>
       <header className="flex items-center justify-between">
-        {wordInfo && (
-          <section className="space-y-1">
-            <h2 className="text-[2.75rem] font-bold font-lora">{wordInfo.word}</h2>
-            <p className="text-2xl text-[#b23fea]">{wordInfo.phonetic}</p>
+        {wordInfoData && (
+          <section className="space-y-2">
+            <h2 className="text-4xl 2xl:text-[2.75rem] font-bold font-lora">{wordInfoData.word}</h2>
+            <p className="text-xl 2xl:text-2xl text-[#b23fea]">{wordInfoData.phonetic}</p>
           </section>
         )}
         {(mp3Source && mp3Source?.length > 0) && (
           <button
             onClick={onClick}
-            className="flex justify-center items-center w-12 h-12 rounded-full text-[#b23fea] bg-[#f0d9fb]"
+            className="flex justify-center items-center w-10 h-10 md:w-12 md:h-12 rounded-full text-[#b23fea] bg-[#f0d9fb]"
           >
             {isAudioPlay ? <Pause fill="#b23fea" size={18} /> : <Play fill="#b23fea" size={18} />}
           </button>
