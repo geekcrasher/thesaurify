0c569f04033877ef8bbad1c63411ad93a66e687b
Paulo Lopez <geekcrasher@gmail.com>
2024-04-28 20:36:00 +0800

    feat: added the Toaster component for rendering toast ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import { Toaster } from "@/components/ui/toaster"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <main className="min-h-screen w-full flex flex-col relative">
       <Navbar />
       <Outlet />
+      <Toaster />
     </main>
   )
 }
